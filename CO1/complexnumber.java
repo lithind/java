94f80e7b18ddda736de366060ed1c8292a6fbd30
AswinP2711 <lithind64@gmail.com>
2021-06-09 12:13:22 +0530

    alter

--- CO1/complexnumber.java
+++ CO1/complexnumber.java
@@ -1,19 +1,54 @@
+// class complex
+// {
+//     int image;
+//     int real;
+//     void assign(int a,int b)
+//     {
+//         image=a;
+//         real=b;
+//     }
+//     void add(complex c1,complex c2)
+//     {
+        
+//         System.out.println("addition of two complex number is: "+(c1.real+c2.real)+"+"+(c1.image+c1.image)+" i");
+//     }
+// }
+// public class complexnumber {
+//     public static void main(String[] args) {
+//         complex c1=new complex();
+//         complex c2=new complex();
+
+//         c1.assign(5,7);
+//         c2.assign(5,7);
+
+//         c1.add(c1,c2);
+
+      
+//     }
+//   }
 class complex
 {
     int image;
     int real;
-    void add(int a,int b,int c,int d)
+    complex(int image,int real)
     {
-        real=a+c;
-        image=b+d;
-        System.out.println("addition of two complex number is: "+real+"+"+image+" i");
+        this.image=image;
+        this.real=real;
+    }
+    void add(complex c1,complex c2)
+    {
+        
+        System.out.println("addition of two complex number is: "+(c1.image+c2.image)+"+"+(c1.real+c2.real)+" i");
     }
 }
 public class complexnumber {
     public static void main(String[] args) {
-        complex c1=new complex();
-        c1.add(5,7,9,8);
+        complex c1=new complex(4,5);
+        complex c2=new complex(1,4);
+
+        c1.add(c1,c2);
 
       
     }
   }
+  
--- CO1/lowprice.java
+++ CO1/lowprice.java
@@ -1,22 +1,55 @@
+// class product{
+//     int pcode;
+//     String pname;
+//     double price;
+//     void setvalue(int code,String name,double pri){
+//     pcode=code;
+//     pname=name;
+//     price=pri;    
+//     }
+//     void small(product p1,product p2,product p3)
+//     {
+//         if ((p1.price<p2.price) && (p1.price<p3.price))
+//         {
+//             System.out.println("lowest price is : "+p1.price);
+//         }
+//         else if ((p2.price<p1.price) && (p2.price<p3.price))
+//         {
+//             System.out.println("lowest price is : "+p2.price);
+
+//         }
+//         else
+//         {
+//             System.out.println("lowest price is : "+p3.price);
+
+//         }
+        
+//     }
+// }
+// public class lowprice{
+//     public static void main(String[] args) {
+//         product p1=new product();
+//         product p2=new product();
+//         product p3=new product();
+//         p1.setvalue(1,"camlinbook",30);
+//         p2.setvalue(2,"classmate notebook",10);
+//         p3.setvalue(3,"triveni notes",50);
+//         p3.small(p1,p2,p3);
+        
+      
+//     }
+//   }
 class product{
     int pcode;
     String pname;
     double price;
-}
-public class lowprice{
-    public static void main(String[] args) {
-        product p1=new product();
-        product p2=new product();
-        product p3=new product();
-        p1.pcode=1;
-        p1.pname="camlinbook";
-        p1.price=10;
-        p2.pcode=2;
-        p2.pname="classmate notebook";
-        p2.price=28;
-        p3.pcode=3;
-        p3.pname="triveni notes";
-        p3.price=35;
+    product(int pcode,String pname,double price){
+    this.pcode=pcode;
+    this.pname=pname;
+    this.price=price;    
+    }
+    void small(product p1,product p2,product p3)
+    {
         if ((p1.price<p2.price) && (p1.price<p3.price))
         {
             System.out.println("lowest price is : "+p1.price);
@@ -31,6 +64,15 @@ public class lowprice{
             System.out.println("lowest price is : "+p3.price);
 
         }
+        
+    }
+}
+public class lowprice{
+    public static void main(String[] args) {
+        product p1=new product(1,"camlinbook",30);
+        product p2=new product(2,"classmate notebook",10);
+        product p3=new product(3,"triveni notes",50);
+        p3.small(p1,p2,p3);
         
       
     }
