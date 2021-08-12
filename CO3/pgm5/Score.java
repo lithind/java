1567157d364c12e0f28563b81873d7a4218a97ea
lithind <lithind64@gmail.com>
2021-08-13 00:30:43 +0530

    modified

---
+++ CO3/pgm5/Score.java
@@ -0,0 +1,21 @@
+// Create classes Student and Sports. Create another class Result inherited from Student and
+// Sports. Display the academic and sports score of a student.
+interface Student
+{
+
+}
+interface Sports
+{
+
+} 
+class Result impliments Student,Sports
+{
+
+}
+class Score
+{
+    public static void main(String args[]) {
+    
+        Result R=new Result();
+    }
+}
